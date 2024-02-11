0a6b71c9cfc6a411c0cdeabe14d5149389de9f33
Afzal14786 <mdafzal14777@gmail.com>
2024-02-11 18:19:21 +0530

    Printing A 2D Matriz

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
 class HelloWorld {
+
+    private static void printMatrix2D(int arr[][]) {
+        for(int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] +" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+
+        int row = 3, cols = 5;
+        int matrix[][] = new int[row][cols];
+        printMatrix2D(matrix);
     }
 }
